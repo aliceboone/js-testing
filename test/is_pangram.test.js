26b2a9579b1cbcfa80cbeb5902804ef97a8ff8af
Alice Boone <alicegbonel@gmail.com>
2020-12-13 11:59:14 -0800

    tests in pangram

--- test/is_pangram.test.js
+++ test/is_pangram.test.js
@@ -10,54 +10,79 @@ describe('isPangram()', () => {
     const text = 'the quick brown fox jumps over the lazy dog';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
+    expect(answer).toEqual(true);
   });
 
   test('works with "abcdefghijklmnopqrstuvwxyz"', () => {
     // Arrange
+    const text = 'abcdefghijklmnopqrstuvwxyz';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
-
+    expect(answer).toBe(true);
   });
 
   test("missing character 'x'", () => {
     // Arrange
+    const text = 'the quick brown fo jumps over the lazy dog';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
-
+    expect(answer).toEqual(false);
   });
 
   test('empty sentence', () => {
-    // Arrange
+     // Arrange
+     const text = '';
 
-    // Act
+     // Act
+     const answer = isPangram(text);
 
-    // Assert
+     // Assert
+     expect(answer).toEqual(false);
 
   });
 
   test('pangram with underscores instead of spaces works', () => {
     // Arrange
+    const text = 'the_quick_brown_fox_jumps_over_the_lazy_dog';
 
     // Act
-
+    const answer = isPangram(text);
+    
     // Assert
-
+    expect(answer).toEqual(true);
   });
 
   test('pangram with numbers', () => {
-    // Arrange
+   // Arrange
+   const text = 'the quick brown fox jumps 37 feet over the lazy dog';
 
-    // Act
-
-    // Assert
-
-  });
+   // Act
+   const answer = isPangram(text);
+   
+   // Assert
+   expect(answer).toEqual(true);
+  }); 
 
   // Write your own test case
+  test ('returns true for text with each letter multiple times', () => {
+
+   // Arrange
+   const text = 'abcdefghijklmnopqrstuvwxyz' + 'abcdefghijklmnopqrstuvwxyz';
+
+   // Act
+   const answer = isPangram(text);
+
+   // Assert
+   expect(answer).toBe(true);
+});
+
 });
